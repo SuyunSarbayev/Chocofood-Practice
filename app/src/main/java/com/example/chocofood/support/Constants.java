cb969e74ce8b1170a0b651af00dfa1715138b829
guldanakys <guldanakys@gmail.com>
2019-06-14 16:39:23 +0600

    clean architecture and main design are added

--- app/src/main/java/com/example/chocofood/support/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.example.chocofood.support;
-
-public final class Constants {
-
-    public static final String BASE_URL = "https://testchocofood.kz/debug-api/";
-
-    public static final int LIMIT = 20;
-}
